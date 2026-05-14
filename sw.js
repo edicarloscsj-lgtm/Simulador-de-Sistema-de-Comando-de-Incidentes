ab04da7c898765d284ebd2d219a4fb5a9aff5e05
edicarloscsj-lgtm <edicarloscsj@gmail.com>
2026-05-14 19:37:43 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'simulador-emergencia-v6-modo-local';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./minha-logo.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
